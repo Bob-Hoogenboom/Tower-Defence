1c2c443942b3d842aeea227e97793d520d88520c
Bobby-Boy <marjabob910@gmail.com>
2020-09-22 14:20:29 +0200

    begin waypoint system
    added waypoint assignment for tower defence game.

---
+++ Assets/Scripts/PathFinder/PathFollower.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PathFinder
+{
+    /// <summary>
+    /// De path follower class is verantwoordelijk voor de beweging.
+    /// Deze class zorgt ervoor dat het object (in Tower Defense) vaak een enemy, het path afloopt
+    /// tip: je kunt de transform.LookAt() functie gebruiken en vooruitbewegen.
+    /// </summary>
+    public class PathFollower : MonoBehaviour
+    {   
+
+    public Transform[] Waypoint;
+    public float Speed;
+    public float Arrivalthreshold = 0.1f;
+
+    private int _currentWaypointIndex;
+
+    private void Start()
+    {
+    
+    }
+
+    private void Update()
+    {
+        Vector3 heightOffsetPosition = new Vector3(Waypoint[_currentWaypointIndex].position.x, 
+            transform.position.y, Waypoint[_currentWaypointIndex].position.z);
+        float distance = Vector3.Distance(transform.position, heightOffsetPosition);
+
+        if (distance <= Arrivalthreshold)
+        {
+            if (_currentWaypointIndex == Waypoint.Length-1)
+            {
+                print("Ik ben bij het eindpunt");
+            }
+            else
+            {
+                _currentWaypointIndex += 1;
+            }
+        }
+        else
+        {
+            transform.LookAt(heightOffsetPosition);
+            transform.Translate(Vector3.forward * Speed * Time.deltaTime);    
+        }
+    }
+    }
+}
+
---
+++ Assets/Scripts/Player/Health.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class Health : MonoBehaviour
+    {
+        public int PlayerHP = 100;
+        private int _currentHP;
+
+
+        void Start()
+        {
+            _currentHP = PlayerHP;
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                TakeDamage(20);
+            }
+        }
+
+        void TakeDamage(int damage)
+        {
+            _currentHP =- damage;
+            Debug.Log(_currentHP);
+        }
+    }
+
+}
